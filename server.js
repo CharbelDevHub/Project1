abc1ce96c4bb79d120ea3f73bb4dd51c78ac244a
Charbel Abi Saad <charbel182002@gmail.com>
2024-05-06 17:34:22 +0300

    initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.json({
+        status: 200,
+        message: "Hello World"
+    })
+})
+
+
+app.listen(8081, (req, res) => {
+    console.log("Server running on port 8081")
+})
